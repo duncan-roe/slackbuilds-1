82596bccf21436dd11047ab119f8c94578674a7f
Duncan Roe <duncan_roe@optusnet.com.au>
2022-01-31 09:26:53 +1100

    Initial attempt (4 tries of make)

---
+++ academic/sage/NB.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+set -x
+export VERSION=9.4
+export TEST=yes
+export PDFDOC=yes
+#export OPTIONALSPKG=""
+export INSTALLSCRIPTS=yes
+
+nice --adjustment=19 sh -c ". ./sage.SlackBuild 2>&1" | tee hee64
