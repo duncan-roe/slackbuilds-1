2d5c118c6577f843f956360734d8edf7c993704a
Duncan Roe <duncan_roe@optusnet.com.au>
2022-02-11 12:44:04 +1100

    Install gf2x; unset PDFDOC & INSTALLSCRIPTS; TMP=/tmp/SBo

--- academic/sage/NB.sh
+++ academic/sage/NB.sh
@@ -2,8 +2,8 @@
 set -x
 export VERSION=9.4
 #export TEST=yes
-export PDFDOC=yes
+#export PDFDOC=yes
 #export OPTIONALSPKG=""
-export INSTALLSCRIPTS=yes
+#export INSTALLSCRIPTS=yes
 
 nice --adjustment=19 sh -c ". ./sage.SlackBuild 2>&1" | tee hee64
