2e9972c28735958af8788f00ed7e3830c9ed6d5d
Duncan Roe <duncan_roe@optusnet.com.au>
2022-02-20 14:04:48 +1100

    sage: Attempt PDFDOC=yes again
    pxjahyper.sty was missing. Installed thi and all subsequent
    missing files (not in commit) until got a stack overflow from latex.
    Details in ~root/sage.scr on slk1

--- academic/sage/NB.sh
+++ academic/sage/NB.sh
@@ -2,8 +2,9 @@
 set -x
 export VERSION=9.4
 #export TEST=yes
-#export PDFDOC=yes
+export PDFDOC=yes
 #export OPTIONALSPKG=""
-#export INSTALLSCRIPTS=yes
+export INSTALLSCRIPTS=yes
+export BUILD=2
 
 nice --adjustment=19 sh -c ". ./sage.SlackBuild 2>&1" | tee hee64
